55ccffc0fc6f8a0e81adc82c45283360850651ac
NafizReza01 <nafizreza2001@gmail.com>
2025-03-05 19:21:50 +0600

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Calm_Connect.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
